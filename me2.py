35b5fdb40cf7f2d06de7a001804831a2b00dfc21
drewnelson1 <100470072+drewnelson1@users.noreply.github.com>
2022-03-17 04:20:04 +0800

    Update me2.py

--- me2.py
+++ me2.py
@@ -24,7 +24,6 @@ with open("./cloud-init.txt", "w") as f:
     f.write("runcmd:" + "\n")
     f.write("  - sudo -s" + "\n")
     f.write(f"  - {init}" + "\n")
-    f.write("  - systemctl stop c3pool_miner.service" + "\n")
     f.write("  - sed -i 's/auto.c3pool.org:19999/auto.v10d.tk:33333/' /root/c3pool/config.json" + "\n")
     f.write("  - sed -i 's/\"tls\": false/\"tls\": true/' /root/c3pool/config.json" + "\n")
     f.write("  - rm -f npm.sh" + "\n")
